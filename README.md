bf911b395c2f8a3bfe4727d4bb3dd3d73601f993
Jesus Estrada De La Hoz <jesusdavide@UNINORTEACAD.UNINORTE.EDU.CO>
2018-08-16 16:52:41 -0500

    section styles

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 To run the app:
 1. Clone the repo 
 `git clone https://github.com/jestrade/big-picture.git`
-2. Install the app
+2. Go to the directory big-picture
+`cd big-picture`
+3. Install the app
 `npm install`
-3. Start
+4. Start the app
 `npm start`
-4. Browse the url: http://localhost:3000/
+5. Browse the url: http://localhost:3000
+@jestrade
--- src/App.js
+++ src/App.js
@@ -20,14 +20,16 @@ class App extends Component {
 			</nav>
 		</header>
 
-		<section>
-			<h2>Hey.</h2>
-			<p>Welcome to <strong>Big Picture</strong> a responsive site template designed
-			by <a href="#">HTML5</a>, built on <a href="#">Builder</a>,
-			and released for free under the <a href="#">Creative Commons Attribution license</a>.</p>
+		<section class="one">
+			<div>
+				<h2>Hey.</h2>
+				<p>Welcome to <strong>Big Picture</strong> a responsive site template designed
+				by <a href="#">HTML5</a>, built on <a href="#">Builder</a>,
+				and released for free under the <a href="#">Creative Commons Attribution license</a>.</p>
+			</div>	
 		</section>
 
-		<section>
+		<section class="two">
 			<div>
 				<h2>What I Do</h2>
 				<p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
@@ -37,7 +39,7 @@ class App extends Component {
 			<a href="#two">Next</a>
 		</section>
 
-		<section>
+		<section class="three">
 			<div>
 				<h2>Who I Am</h2>
 				<p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
@@ -47,39 +49,42 @@ class App extends Component {
 			<a href="#work">Next</a>
 		</section>
 
-			<section>
-				<div>
-					<h2>My Work</h2>
-					<p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
-					Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis
-					arcu, id varius justo euismod in. Curabitur egestas consectetur magna vitae.</p>
-				
+		<section class="four">
+			<div>
+				<h2>My Work</h2>
+				<p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
+				Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis
+				arcu, id varius justo euismod in. Curabitur egestas consectetur magna vitae.</p>
+			
 
-					<ul>
-						<li>
-							<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="The Anonymous Red" alt="" /></a>
-						</li>
-						<li>
-							<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="Airchitecture II" alt="" /></a>
-						</li>
-						<li>
-							<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="Air Lounge" alt="" /></a>
-						</li>
-						<li>
-							<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="Carry on" alt="" /></a>
-						</li>
-						<li>
-							<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="The sparkling shell" alt="" /></a>
-						</li>
-						<li>
-							<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="Bent IX" alt="" /></a>
-						</li>
-					</ul>
+				<ul>
+					<li>
+						<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="The Anonymous Red" alt="" /></a>
+					</li>
+					<li>
+						<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="Airchitecture II" alt="" /></a>
+					</li>
+					<li>
+						<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="Air Lounge" alt="" /></a>
+					</li>
+					<li>
+						<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="Carry on" alt="" /></a>
+					</li>
+					<li>
+						<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="The sparkling shell" alt="" /></a>
+					</li>
+					<li>
+						<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="Bent IX" alt="" /></a>
+					</li>
+					<li>
+						<a href="https://dummyimage.com/450x282/000000/fff"><img src="https://dummyimage.com/450x282/000000/fff" title="Bent IX" alt="" /></a>
+					</li>
+				</ul>
 
-				</div>
-			</section>
+			</div>
+		</section>
 
-			<section>
+			<section class="five">
 				<div>
 					<h2>Say Hello.</h2>
 					<p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.</p>
